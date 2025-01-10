a0c243beb714bd4adc961b41ac8ea7bfb5c74821
Iulian Tiganasu <csman_ul@yahoo.com>
2025-01-10 18:15:16 +0200

    0.0.11+8 - SQLFrom mutable tableNameOrExpr

--- lib/src/sql_from.dart
+++ lib/src/sql_from.dart
@@ -3,7 +3,7 @@ import 'package:dart_sql/src/sql_order_by.dart';
 import 'package:dart_sql/src/sql_writer.dart';
 
 class SQLFrom extends SQLExpression {
-  final String tableNameOrExpr;
+  String tableNameOrExpr;
   SQLFrom(this.tableNameOrExpr, {SQLWriter? parent}) : super(parent: parent);
 
   SQLOrderBy orderBy(List<String> columns) => SQLOrderBy(columns, parent: this);
